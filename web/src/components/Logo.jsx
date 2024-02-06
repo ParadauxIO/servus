237031961312e004e6deb8f7aab23911064ba741
Rían Errity <rian@paradaux.io>
2024-02-06 00:31:13 +0000

    Getting web setup with my vite boilerplate.

---
+++ web/src/components/Logo.jsx
@@ -0,0 +1,7 @@
+import logo from "../assets/servus.webp";
+
+export default function Logo() {
+    return (
+        <img src={logo} alt="The Servus Logo."/>
+    )
+}
--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./styles/fonts.scss";
+import "./styles/reset.scss";
+import "./styles/utilities.scss";
+import './styles/global.scss';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
