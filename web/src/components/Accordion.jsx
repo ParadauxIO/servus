5e0cf4d467cdbd6d7ff8e3fbf203bd4990676a99
Rían Errity <rian@paradaux.io>
2024-02-06 01:36:30 +0000

    Add jsdoc

--- web/src/components/Accordion.jsx
+++ web/src/components/Accordion.jsx
@@ -1,14 +1,25 @@
 import { useState } from "react";
 import "./Accordion.scss";
 
-export default function Accordion({ title, children }) {
+/**
+ * @param title The title of the accordion
+ * @param subtitle A component which is optionally displayed next to the plus/minus icon.
+ * @param children The content displayed while the accordion is open.
+ * @author Rían Errity
+ * Represents an accordion component, which "expands" when clicked to show hidden content.
+ */
+export default function Accordion({ title, Subtitle, children }) {
     const [isShown, setIsShown] = useState(false);
 
     return (
         <div className="accordion" onClick={() => setIsShown(prev => !prev)}>
             <div className="accordion-title">
                 <h2>{title}</h2>
-                <h2>{isShown ? "-" : "+"}</h2>
+                <div>
+                    {Subtitle && <Subtitle/>}
+                    <h2>{isShown ? "-" : "+"}</h2>
+                </div>
+                
             </div>
             <div className={"accordion-content" + (isShown ? "" : " hidden")}>
                 {children}
